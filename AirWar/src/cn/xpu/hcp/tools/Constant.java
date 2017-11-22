e973c9884b856715504295577128a56f15a1e1e1
huchengpeng <2233749193@qq.com>
2017-11-22 17:52:47 +0800

    height=764

--- AirWar/src/cn/xpu/hcp/tools/Constant.java
+++ AirWar/src/cn/xpu/hcp/tools/Constant.java
@@ -1,6 +1,6 @@
 package cn.xpu.hcp.tools;
 
 public class Constant {
-	public static  final int GAME_WIDTH=640;
-	public static  final int GAME_HEIGHT=700;
+	public static  final int GAME_WIDTH=590;
+	public static  final int GAME_HEIGHT=764;
 }
