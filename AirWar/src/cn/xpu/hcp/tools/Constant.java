60b01b7e8ab4ae281caf28ee3adbfef05841437b
huchengpeng <2233749193@qq.com>
2017-11-22 16:49:10 +0800

    设置窗体大小

--- AirWar/src/cn/xpu/hcp/tools/Constant.java
+++ AirWar/src/cn/xpu/hcp/tools/Constant.java
@@ -1,6 +1,6 @@
 package cn.xpu.hcp.tools;
 
 public class Constant {
-	public static  final int GAME_WIDTH=1366;
-	public static  final int GAME_HEIGHT=768;
+	public static  final int GAME_WIDTH=590;
+	public static  final int GAME_HEIGHT=765;
 }
